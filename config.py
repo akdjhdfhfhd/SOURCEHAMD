6fa1a6bb69ed462b15a8ae9ecb425c4e5e11a536
ahmed11 <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-04-14 02:02:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -13,8 +13,7 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "ᴀɴᴏɴ ダ ᴍᴜsɪᴄ") 
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID")) 
 OWNER_ID = list(map(int, getenv("OWNER_ID", "5167919285").split()))
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
